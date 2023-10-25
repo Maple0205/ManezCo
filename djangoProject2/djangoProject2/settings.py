6fa58564fed26b2dd13fda573ad2038bb29f3e61
Nelson <z5382667@student.unsw.edu.au>
2023-10-26 10:13:28 +1100

    modify host

--- djangoProject2/djangoProject2/settings.py
+++ djangoProject2/djangoProject2/settings.py
@@ -172,4 +172,6 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_ROOT = BASE_DIR / 'file/image'
 MEDIA_URL = 'file/image/'
 
-# ALLOWED_HOSTS = ['https://manez-backend.onrender.com']
+# settings.py
+ALLOWED_HOSTS = ['manezco.onrender.com', 'your_other_allowed_hosts.com']
+
